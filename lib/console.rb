1570f95622255f62d50f046582d2e1d3d994ea9f
Nobuyoshi Nakada <nobu@ruby-lang.org>
2010-04-06 19:05:18 +0900

    * lib/console.rb: implemented.

--- lib/console.rb
+++ lib/console.rb
@@ -7,25 +7,58 @@ rescue LoadError
     require 'rbconfig'
     class IO
       module Console
+        ENABLE_ECHO_INPUT = 4
         extend DL::Importable
         dlload "kernel32", Config::CONFIG["RUBY_SO_NAME"]
         extern "DWORD SetConsoleMode(HANDLE, DWORD)"
         extern "DWORD GetConsoleMode(HANDLE, DWORD*)"
         extern "HANDLE rb_w32_get_osfhandle(int)"
       end
+      def echo=(enable)
+        echo_input = Console::ENABLE_ECHO_INPUT
+        ttymode {|m| enable ? m | echo_input : m & ~echo_input}
+      end
+      def echo?
+        echo_input = Console::ENABLE_ECHO_INPUT
+        (ttymode {|m| break m} & echo_input) != 0
+      end
+      def raw
+        mode = nil
+        ttymode {|m| mode = m; 0}
+        yield(self)
+      ensure
+        Console.setConsoleMode(handle, mode) if mode
+      end
+      def noecho
+        mode = nil
+        ttymode {|m| mode = m; m & ~Console::ENABLE_ECHO_INPUT}
+        yield(self)
+      ensure
+        Console.setConsoleMode(handle, mode) if mode
+      end
+      def getch
+        raw {getc}
+      end
+      def iflush
+      end
+      def oflush
+      end
+      def ioflush
+      end
+      private
       def handle
         Console.rb_w32_get_osfhandle(fileno)
       end
-      def ttymode(m)
+      def ttymode
         h = handle
         t = malloc(sizeof("L"))
-        Console.getConsoleMode(h, t) or raise
+        Console.getConsoleMode(h, t).nonzero? or raise
         t = yield(t.to_a("L")[0])
-        Console.setConsoleMode(h, t) or raise
+        Console.setConsoleMode(h, t).nonzero? or raise
       end
     end
-    def File.console
-      @console ||= open("CON$", "r+")
+    def IO.console
+      @console ||= File.open("CON$", "r+")
     end
   end
 end
